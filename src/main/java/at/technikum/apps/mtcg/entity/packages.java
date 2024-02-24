7bbe55fdb12d24ef150aa3ee46c695fb3dc081fe
Martin7Ni <130511748+Martin7Ni@users.noreply.github.com>
2024-02-24 20:31:03 +0100

    Delete src directory

--- src/main/java/at/technikum/apps/mtcg/entity/packages.java
+++
@@ -1,8 +0,0 @@
-package at.technikum.apps.mtcg.entity;
-
-import java.util.ArrayList;
-
-public class packages {
-	private ArrayList<Card> cards;
-	private static int cost = 5;
-}
--- src/main/java/at/technikum/apps/mtcg/repository/BattlesRepository.java
+++
@@ -1,9 +0,0 @@
-package at.technikum.apps.mtcg.repository;
-
-import at.technikum.apps.mtcg.data.Database;
-
-public class BattlesRepository {
-
-	
-	private final Database database = new Database();
-}
